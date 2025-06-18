99e45a338ca8079d6025c04122b45c2b294dcd84
GautamK31 <kamejaliyagauti07@gmail.com>
2025-06-18 17:54:54 +0530

    Added copy clipboard , remove text & remove space button

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,6 +9,17 @@ export default function TextForm(props) {
         let newText = text.toLowerCase();
         setText(newText);
     }
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
+    const handleClearText = () => {
+        setText('');
+    }
+
+    const handleRemoveSpace = () => {
+        let newText = text.replace(/\s+/g, '');
+        setText(newText);
+    }
     const textChange = (event) => {
         setText(event.target.value);
     }
@@ -20,7 +31,10 @@ export default function TextForm(props) {
                 <textarea className="form-control" id="text" rows="10" onChange={textChange} value={ text }></textarea>
             </div>
             <button className='btn btn-primary my-2' onClick={handleUpperCase}>Covert to Uppercase</button>
-            <button className='btn btn-primary mx-3' onClick={handleLowercase}>Covert to Lowercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleLowercase}>Covert to Lowercase</button>
+            <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
+            <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
+            <button className='btn btn-primary mx-2' onClick={handleRemoveSpace}>Remove Space</button>
         </div>
     )
 }
